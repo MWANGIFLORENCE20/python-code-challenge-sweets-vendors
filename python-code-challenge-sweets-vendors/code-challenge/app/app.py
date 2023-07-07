c494fdba9aa57a7c049a211e9abc0abf3a850dd1
florence wangechi <florencewangechim@gmail.com>
2023-07-08 00:14:02 +0300

    vendor-sweet

--- python-code-challenge-sweets-vendors/code-challenge/app/app.py
+++ python-code-challenge-sweets-vendors/code-challenge/app/app.py
@@ -1,22 +1,97 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
-
-from models import db, Vendor
+from flask_restful import Api, Resource
+from models import db, Vendor, Sweet, VendorSweet
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
+api = Api(app)
 
 @app.route('/')
 def home():
-    return ''
+    return 'THE CANDY SHOP'
 
+class VendorResource(Resource):
+    def get(self):
+
+        vendor = Vendor.query.all()
+        vend_list = [item.to_dict() for item in vendor]
+        return make_response(jsonify(vend_list), 200)
+    
+   
+api.add_resource(VendorResource, "/vendor")
+
+
+class VendorResourceID(Resource):
+    def get(self, id):
+
+       vendor = Vendor.query.filter_by(id=id).first()
+
+       return make_response(jsonify(vendor), 200) 
+       
+    
+api.add_resource(VendorResourceID, "/vendor/<int:id>")
+
+class SweetResource(Resource):
+    def get (self):
+
+        sweet = Sweet.query.all()
+
+        sweet_list = [item.to_dict() for item in sweet]
+
+        return make_response(jsonify(sweet_list), 200)
+
+api.add_resource(SweetResource, "/sweet")
+
+class SweetResourceById(Resource):
+    def get (self, id):
+
+        sweet = Sweet.query.filter_by(id = id).first()
+
+        return make_response(jsonify(sweet), 200)
+
+api.add_resource(SweetResourceById, "/sweet/<int:id>")
+
+
+class VendorSweetResource(Resource):
+    def post(self):
+
+        new_vendorsweet = VendorSweet(
+            price = vendorsweet["price"],
+            vendor_id = vendorsweet["vendor_id"],
+            sweet_id = vendorsweet['sweet_id']
+        )
+
+        db.session.add(new_vendorsweet)
+        db.session.commit()
+
+        vendorsweet = new_vendorsweet.to_dict()
+
+        return make_response(jsonify(vendorsweet), 201)
+
+
+    def delete(self, id):
+        record = (Vendor.query.filter_by(id=id)).first()
+
+        db.session.delete(record)
+        db.session.commit()
+        
+        response = {
+            "message":"message is deleted"
+        }
+
+
+        return make_response(jsonify(response), 200)
+
+        
+api.add_resource(VendorSweetResource, "/vendor_sweet/<int:id>")
 
 if __name__ == '__main__':
     app.run(port=5555)
--- python-code-challenge-sweets-vendors/code-challenge/app/models.py
+++ python-code-challenge-sweets-vendors/code-challenge/app/models.py
@@ -1,10 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Vendor(db.Model):
-    __tablename__ = 'vendor'
+class Vendor(db.Model, SerializerMixin):
+    __tablename__ = 'vendors'
+
+    serialize_rules = ("-vendor_sweets.vendor",)
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
-# add any models you may need. 
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    vendor_sweets = db.relationship("VendorSweet", backref="vendor")
+
+    def __repr__(self):
+        return f'<Vendor {self.name} >'
+
+
+class Sweet(db.Model, SerializerMixin):  
+    __tablename__ = "sweets"
+
+    serialize_rules = ("-vendor_sweets.sweet",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now()) 
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    vendor_sweets= db.relationship('VendorSweet', backref='sweet')
+    
+
+class VendorSweet(db.Model, SerializerMixin):
+    __tablename__= "vendor_sweets"
+
+    serialize_rules = ("-vendor.vendor_sweets","-sweet.vendor_sweets",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+#relationships
+    sweet_id = db.Column(db.Integer, db.ForeignKey("sweets.id"))
+    vendor_id = db.Column(db.Integer,db.ForeignKey("vendors.id"))
+
+    def __repr__(self):
+        return f'<VendorSweet ({self.id})>'
